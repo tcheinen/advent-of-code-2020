7be5e167077a40ef58c94595464c21f9034cf2c6
Teddy Heinen <teddy@heinen.dev>
2020-12-01 14:47:56 -0600

    day 1 - gotta go fast

--- src/day1.rs
+++ src/day1.rs
@@ -1,40 +1,37 @@
-use itertools::Itertools;
+use std::collections::HashSet;
 
 /// https://adventofcode.com/2020/day/1
-
 
 #[aoc_generator(day1)]
 pub fn generator(input: &str) -> Vec<u32> {
     input.split("\n").flat_map(|x| x.parse()).collect()
 }
 
-/// We're solving this the lazy way!  Pick every pair (a,b) and find the first one that sums to 2020
-/// We can optimize this significantly by sorting it in ascending order first
-/// Our pairs are weighted towards smaller (a,b) because obviously a pair where both are greater than 1010 can't sum to 2020
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &[u32]) -> u32 {
+    let nums: HashSet<u32> = input.iter().cloned().collect();
     input
         .iter()
-        .sorted()
-        .combinations(2)
-        .map(|x| (*x[0], *x[1]))
-        .find(|(a, b)| a + b == 2020)
-        .map(|(a, b)| a * b)
+        .find(|x| nums.contains(&(2020 - **x)))
+        .map(|x| x * (2020 - *x))
         .unwrap()
 }
 
-/// We're solving this the lazy way!  Pick every pair (a,b,c) and find the first one that sums to 2020
-/// We can optimize this significantly by sorting it in ascending order first
-/// Our pairs are weighted towards smaller (a,b,c) because obviously a pair where all are greater than 673 can't sum to 2020
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &[u32]) -> u32 {
+    let nums: HashSet<u32> = input.iter().cloned().collect();
     input
         .iter()
-        .sorted()
-        .combinations(3)
-        .map(|x| (*x[0], *x[1], *x[2]))
-        .find(|(a, b, c)| a + b + c== 2020)
-        .map(|(a, b, c)| a * b * c)
+        .flat_map(|a| {
+            input
+                .iter()
+                .filter(|x| **x < (2020 - *a))
+                .map(|x| (a, x))
+                .collect::<Vec<_>>()
+                .into_iter()
+        })
+        .find(|(a, b)| nums.contains(&(2020 - **a - **b)))
+        .map(|(a, b)| *a * *b * (2020 - *a - *b))
         .unwrap()
 }
 
@@ -52,7 +49,6 @@ mod tests {
 1456";
         assert_eq!(514579, solve_part1(&generator(provided)));
     }
-
 
     #[test]
     fn it_works_part2() {
